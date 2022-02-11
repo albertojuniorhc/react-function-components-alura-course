871b536c5c26721693725b9adc77aea9f3423adb
Alberto Jr. (Beto) <albertojuniorhc@gmail.com>
2022-02-11 17:07:01 -0300

    Class04 (#1)
    * finished class 3 and beginning class 4
    
    * finished class4

--- register-form/src/App.js
+++ register-form/src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import "./App.css";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { Container, Typography } from "@mui/material";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
     <Container component="article" maxWidth="sm">
-      <Typography variant="h3" component="h1" align="center">Register Form</Typography>
-      <RegisterForm />
+      <Typography variant="h3" component="h1" align="center">
+        Register Form
+      </Typography>
+      <RegisterForm submitForm={onSubmitForm} />
     </Container>
   );
+}
+
+function onSubmitForm(dataForm) {
+  console.log(dataForm);
 }
 
 export default App;
--- register-form/src/components/RegisterForm/RegisterForm.jsx
+++ register-form/src/components/RegisterForm/RegisterForm.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import {
-  Button,
-  TextField,
-  Switch,
-  FormControlLabel,
-  Container,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
-function RegisterForm() {
+function RegisterForm({ submitForm }) {
+  const [name, setName] = useState("Alberto");
+  const [lastName, setLastName] = useState("Junior");
+  const [cpf, setCpf] = useState("121.321.538-21");
+  const [promo, setPromo] = useState(true);
+  const [news, setNews] = useState(true);
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        submitForm({ name, lastName, cpf, promo, news });
+      }}
+    >
       <TextField
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
         id="name"
         label="Name"
         variant="outlined"
@@ -18,6 +26,10 @@ function RegisterForm() {
         margin="normal"
       />
       <TextField
+        value={lastName}
+        onChange={(event) => {
+          setLastName(event.target.value);
+        }}
         id="lastname"
         label="Last Name"
         variant="outlined"
@@ -25,6 +37,10 @@ function RegisterForm() {
         margin="normal"
       />
       <TextField
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
         id="cpf"
         label="CPF"
         variant="outlined"
@@ -34,12 +50,30 @@ function RegisterForm() {
 
       <FormControlLabel
         label="Promo?"
-        control={<Switch name="promo" defaultChecked color="primary" />}
+        control={
+          <Switch
+            onChange={(event) => {
+              setPromo(event.target.checked);
+            }}
+            name="promo"
+            checked={promo}
+            color="primary"
+          />
+        }
       />
 
       <FormControlLabel
         label="News?"
-        control={<Switch name="news" defaultChecked color="primary" />}
+        control={
+          <Switch
+            onChange={(event) => {
+              setNews(event.target.checked);
+            }}
+            name="news"
+            checked={news}
+            color="primary"
+          />
+        }
       />
 
       <Button variant="contained" color="primary" type="submit">
